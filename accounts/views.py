436ac8cb397bfd3924bbd759ac35f76b43337244
Lukas Graig <graig45@gmail.com>
2021-09-22 12:57:20 -0400

    created user_profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserModel
+from django.http.response import Http404
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def user_profile(request, user):
+    try:
+        user = UserModel.objects.get(username=user)
+    except UserModel.DoesNotExist:
+        raise Http404(f"{user} does not exist")
+    return render(request, 'registration/profile.html', {'user': user})
