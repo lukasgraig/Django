af2ab69815ee58dce62392a165187ef69610d0f6
Lukas Graig <graig45@gmail.com>
2021-09-24 12:43:41 -0400

    added logout feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('profile/<str:user>', views.user_profile, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
